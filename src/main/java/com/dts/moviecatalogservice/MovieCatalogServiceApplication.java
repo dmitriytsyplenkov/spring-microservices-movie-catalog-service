809df94e3b7cd619d94c6cfef810e45e8e834595
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-06-01 02:13:55 +0300

    slightly changed stuff

--- src/main/java/com/dts/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/dts/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
